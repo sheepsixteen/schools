b6b34f030289f49ba5d25dac3f23f8f461b42942
Ori Marash <ori@marash.net>
2020-11-10 10:14:42 +0000

    Add lesson 2 and lesson 3
    Thank you Ido Zeisel!!!
    
    (there are also a few style changes in this commit)

--- _plugins/rtl_code.rb
+++ _plugins/rtl_code.rb
@@ -1,6 +1,16 @@
 module Kramdown
   module Converter
     class SheepHtml < Html
+      def convert_codespan(el, indent)
+        if not el.attr['dir']
+          # Add dir="ltr" to the element
+          el.attr['dir'] = 'ltr'
+        end
+
+        # Do whatever magic kramdown does
+        super(el, indent)
+      end
+
       def convert_codeblock(el, indent)
         if not el.attr['dir']
           # Add dir="ltr" to the element
